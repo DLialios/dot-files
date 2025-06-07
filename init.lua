edb1ee5bebae306d007e1be15b974018208131fa
Dimitri Lialios <lialios.dimitri@gmail.com>
2025-06-07 15:43:44 -0400

    Update

--- init.lua
+++ init.lua
@@ -1,185 +1,278 @@
--- Automatic installation of vim-plug
---------------------------------------------------------------------------------
-local url =
-'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-local curl_cmd = 'silent !curl --create-dirs -fLo'
+-- Options --
+vim.opt.background = 'dark'
+vim.opt.termguicolors = true
+vim.cmd('highlight Normal guibg=Black guifg=White')
 
-local data_dir = vim.fn.stdpath('data')
-local script_dir = '/site/autoload/plug.vim'
-local full_path = data_dir .. script_dir
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
-local requires_install = vim.fn.empty(vim.fn.glob(full_path)) == 1
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
-if requires_install then
-    vim.cmd(string.format('%s %s %s', curl_cmd, full_path, url))
-    vim.o.runtimepath = vim.o.runtimepath -- required because of nvim bug
-    vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
-end
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 4
 
+vim.opt.number = true
+vim.opt.relativenumber = true
 
--- Aliases
---------------------------------------------------------------------------------
-local global = vim.g
-local o = vim.opt
-local map = function(mode, lhs, rhs)
-    vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true })
-end
+vim.opt.colorcolumn = '80'
+vim.opt.signcolumn = 'yes'
+vim.opt.cursorline = true
 
+vim.opt.laststatus = 2
+vim.opt.ruler = true
+vim.opt.showmode = true
+vim.opt.showcmd = true
 
--- Options
---------------------------------------------------------------------------------
-o.background = 'dark'
-o.termguicolors = true
-o.number = true
-o.relativenumber = true
-o.scrolloff = 4
-o.cursorline = true
-o.ignorecase = true
-o.smartcase = true
-o.updatetime = 250
-o.timeoutlen = 300
-o.splitright = true
-o.splitbelow = true
-o.list = false
-o.listchars = { eol = '$', tab = '>>', space = '_', trail = '·', nbsp = '␣' }
-o.fileformats = 'unix,dos'
-o.foldmethod = 'expr'
-o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-o.foldenable = true
-o.foldlevel = 99
-o.colorcolumn = '80'
-o.signcolumn = 'yes'
-o.showmode = false
-o.completeopt = { 'menu' }
-o.mouse = 'a'
-o.undofile = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.timeout = true
+vim.opt.ttimeoutlen = 20
+vim.opt.ttimeout = true
 
+vim.opt.list = false
+vim.opt.listchars = {
+    eol = '$',
+    tab = '>>',
+    space = '_',
+    trail = '·',
+    nbsp = '␣'
+}
 
--- Misc
---------------------------------------------------------------------------------
-global.mapleader = ' '
-global.maplocalleader = ' '
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldmethod = 'expr'
+vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
+
+vim.opt.undofile = true
+vim.opt.undolevels = 2000
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.autoread = true
+vim.opt.hidden = true
+vim.opt.fileformats = 'unix,dos'
+vim.opt.incsearch = true
+
+vim.opt.mouse = 'a'
+vim.cmd [[
+aunmenu PopUp.-1-
+aunmenu PopUp.-2-
+aunmenu PopUp.How-to\ disable\ mouse
+]]
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
     callback = function()
         vim.highlight.on_yank()
-    end,
+    end
 })
 
 vim.cmd [[
-aunmenu PopUp.How-to\ disable\ mouse
-aunmenu PopUp.-1-
+filetype on
+filetype plugin on
+filetype indent on
+syntax off
 ]]
 
 
--- Plugins
---------------------------------------------------------------------------------
-local Plug = vim.fn['plug#']
+-- Mappings --
+local map = function(mode, lhs, rhs)
+    vim.keymap.set(mode, lhs, rhs, {
+        noremap = true,
+        silent = true
+    })
+end
 
-vim.call('plug#begin')
-Plug('nvim-lua/plenary.nvim')
-Plug('nvim-tree/nvim-web-devicons')
-Plug('folke/tokyonight.nvim')
-Plug('tpope/vim-sleuth')
-Plug('nvim-lualine/lualine.nvim')
-Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
-Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.8' })
-Plug('nvim-telescope/telescope-fzf-native.nvim', { ['do'] = 'make' })
--- Plug('neovim/nvim-lspconfig')
-vim.call('plug#end')
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-
--- Plugin Config
---------------------------------------------------------------------------------
-require('telescope').setup { }
-require('telescope').load_extension('fzf')
-require('lualine').setup { options = { theme = 'tokyonight-storm', } }
-require('nvim-web-devicons').setup { }
-vim.cmd [[ colorscheme tokyonight-storm ]]
-require('nvim-treesitter.configs').setup {
-    ensure_installed = {
-        'c', 'cpp', 'python', 'lua', 'vim', 'vimdoc', 'query',
-        'markdown', 'markdown_inline'
-    },
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-    indent = {
-        enable = false
-    }
-}
-
-
--- Mappings
---------------------------------------------------------------------------------
-map('n', '<Esc>', '<cmd>nohlsearch<CR>')
-map('t', '<Esc><Esc>', '<C-\\><C-n>')
-
-map('n', '<Leader>e', '<cmd>e $MYVIMRC<CR>')
-map('n', '<Leader>t', '<cmd>tabnew<CR>')
-map('n', '<Leader>d', '<C-]>')
+map('n', '<ScrollWheelUp>', '<C-Y>')
+map('n', '<ScrollWheelDown>', '<C-E>')
 
 map('n', '<Leader>h', '<cmd>cfirst<CR>')
 map('n', '<Leader>j', '<cmd>cn<CR>')
 map('n', '<Leader>k', '<cmd>cp<CR>')
 map('n', '<Leader>l', '<cmd>clast<CR>')
-map('n', '<Leader>g', '<cmd>cclose<CR>')
 
-map('n', '<Leader>p', "<cmd>lua require('telescope.builtin').find_files()<CR>")
-map('n', '<Leader>P',
-"<cmd>lua require('telescope.builtin').find_files({ " ..
-"hidden = true, no_ignore = true, no_ignore_parent = true })<CR>")
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+map('n', '<Leader>t', '<cmd>tabnew<CR>')
+map('n', '<Leader>q', '<cmd>q<CR>')
+map('n', '<Leader>e', '<cmd>e $MYVIMRC<CR>')
+map('n', '<Leader>d', '<C-]>')
 
-map('n', '<Leader>f', "<cmd>lua require('telescope.builtin').grep_string()<CR>")
-map('n', '<Leader>F',
-"<cmd>lua require('telescope.builtin').grep_string({ " ..
-"additional_args = function() " ..
-    "return {'--hidden','--no-ignore-vcs'} end })<CR>"
+
+-- Plugins --
+vim.call('plug#begin')
+
+vim.fn['plug#']('tpope/vim-sleuth')
+
+vim.fn['plug#']('nvim-treesitter/nvim-treesitter', {
+    ['do'] = ':TSUpdate'
+})
+vim.fn['plug#']('nvim-treesitter/nvim-treesitter-textobjects')
+vim.fn['plug#']('EdenEast/nightfox.nvim')
+
+vim.fn['plug#']('nvim-lua/plenary.nvim')
+vim.fn['plug#']('nvim-telescope/telescope.nvim', {
+    ['tag'] = '0.1.8'
+})
+vim.fn['plug#']('nvim-telescope/telescope-fzf-native.nvim', {
+    ['do'] = 'make'
+})
+
+vim.fn['plug#']('neovim/nvim-lspconfig')
+
+vim.fn['plug#']('eandrju/cellular-automaton.nvim', {
+    ['on'] = 'CellularAutomaton'
+})
+
+vim.call('plug#end')
+
+
+-- Plugin Config --
+map('n', '<Leader>fml', '<cmd>CellularAutomaton make_it_rain<CR>')
+
+require('telescope').setup()
+require('telescope').load_extension('fzf')
+
+vim.cmd('colorscheme carbonfox')
+require('nvim-treesitter.configs').setup {
+    ensure_installed = {
+        'c',
+        'lua',
+        'vim',
+        'vimdoc',
+        'query',
+        'markdown',
+        'markdown_inline'
+    },
+    sync_install = true,
+    auto_install = true,
+    ignore_install = { },
+    highlight = {
+        enable = true,
+        disable = { },
+        additional_vim_regex_highlighting = false
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner'
+            },
+            selection_modes = {
+                ['@function.outer'] = 'V',
+                ['@function.inner'] = 'V'
+            },
+            include_surrounding_whitespace = true,
+        }
+    }
+}
+
+
+-- Telescope --
+local function buffers_action()
+    require('telescope.builtin').buffers()
+end
+
+local function tags_action()
+    require('telescope.builtin').tags()
+end
+
+local function find_files_action()
+    require('telescope.builtin').find_files()
+end
+
+local function live_grep_action()
+    require('telescope.builtin').live_grep()
+end
+
+local function grep_string_action()
+    require('telescope.builtin').grep_string()
+end
+
+local function find_files_hidden_action()
+    require('telescope.builtin').find_files({
+        hidden = true,
+        no_ignore = true,
+        no_ignore_parent = true
+    })
+end
+
+local function live_grep_hidden_action()
+    require('telescope.builtin').live_grep({
+        additional_args = {
+            '--hidden',
+            '--no-ignore-vcs'
+        }
+    })
+end
+
+local function grep_string_hidden_action()
+    require('telescope.builtin').grep_string({
+        additional_args = {
+            '--hidden',
+            '--no-ignore-vcs'
+        }
+    })
+end
+
+local telescope_mappings = {
+    { mode = 'n', key = '<Leader>b', action = buffers_action },
+    { mode = 'n', key = '<Leader>a', action = tags_action },
+    { mode = 'n', key = '<Leader>p', action = find_files_action },
+    { mode = 'n', key = '<Leader>s', action = live_grep_action },
+    { mode = 'n', key = '<Leader>f', action = grep_string_action },
+    { mode = 'n', key = '<Leader>P', action = find_files_hidden_action },
+    { mode = 'n', key = '<Leader>S', action = live_grep_hidden_action },
+    { mode = 'n', key = '<Leader>F', action = grep_string_hidden_action }
+}
+
+for _, mapping in ipairs(telescope_mappings) do
+    map(mapping.mode, mapping.key, mapping.action)
+end
+
+
+-- LSP --
+local function stop_lsp()
+    vim.lsp.stop_client(vim.lsp.get_clients(), true)
+end
+
+map('n', '<Leader>r', stop_lsp)
+map('n', '<Leader>?', '<cmd>checkhealth vim.lsp<CR>')
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(ev)
+        vim.lsp.completion.enable(
+            true,
+            ev.data.client_id,
+            ev.buf)
+        end
+    }
 )
 
-map('n', '<Leader>s', "<cmd>lua require('telescope.builtin').live_grep()<CR>")
-map('n', '<Leader>S',
-"<cmd>lua require('telescope.builtin').live_grep({ " ..
-"additional_args = function() " ..
-    "return {'--hidden','--no-ignore-vcs'} end })<CR>"
-)
 
-map('n', '<Leader>b', "<cmd>lua require('telescope.builtin').buffers()<CR>")
-map('n', '<Leader>a', "<cmd>lua require('telescope.builtin').tags()<CR>")
+-- Servers --
+vim.lsp.config('clangd', {
+    cmd = {
+        'clangd',
+        '--background-index',
+        '--query-driver=C:\\Users\\dimitri\\.platformio\\packages\\toolchain-xtensa-esp-elf\\bin\\xtensa-esp32s3-elf-*.exe'
+    },
+})
+
+vim.lsp.config('rust_analyzer', {
+    settings = {
+        ['rust-analyzer'] = { }
+    }
+})
 
 
--- LSP
---------------------------------------------------------------------------------
-
--- local on_attach = function(client, bufnr)
---     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
---     local opts = { noremap = true, silent = true }
--- 
---     vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
---     vim.diagnostic.enable(true, { bufnr = bufnr })
--- 
---     buf_set_keymap('n', 'gd', "<cmd>lua require('telescope.builtin').lsp_definitions()<CR>", opts)
---     buf_set_keymap('n', 'gf', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
---     buf_set_keymap('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
---     buf_set_keymap('n', 'gH', "<cmd>lua require('telescope.builtin').lsp_references()<CR>", opts)
---     buf_set_keymap('n', 'grn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
---     buf_set_keymap('i', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
---     buf_set_keymap('n', '<Leader>e', "<cmd>lua require('telescope.builtin').diagnostics({ bufnr = 0 })<CR>", opts)
---     buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
---     buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
--- end
--- 
--- vim.lsp.set_log_level('OFF')
--- 
+-- Enable --
 -- vim.lsp.enable('clangd')
--- vim.lsp.config('clangd', {
---     cmd = {
---         'clangd',
---         '--background-index',
---     },
---     on_attach = on_attach
--- })
+-- vim.lsp.enable('rust_analyzer')
+
