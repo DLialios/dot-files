4dfae224b99ad6b18a3f441de3ecbc05fadd5331
Dimitri Lialios <djlialios@usdi.com>
2025-06-22 14:53:42 -0400

    updates

--- windows/alias/esp-bt.bat
+++ windows/alias/esp-bt.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set "B=C:\Users\dimitri\.platformio\packages\toolchain-xtensa-esp-elf\bin"
-set "E=C:\Users\dimitri\Desktop\projects\dunamis\GEN_2\gen2_esp\.pio\"
+set "E=C:\Users\dimitri\Desktop\projects\dunamis\GEN_2\esp32\.pio\"
 set "E=%E%build\dual80\firmware.elf"
 
 "%B%\xtensa-esp32s3-elf-addr2line.exe" -aspCfire "%E%" %*
